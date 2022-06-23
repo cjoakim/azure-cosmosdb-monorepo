92ff4a96eeb1124b974abcb2cd52ee9f33ddb75d
Chris Joakim <christopher.joakim@gmail.com>
2022-06-23 17:35:15 -0400

    added apis/sql/python/alt_graph/data/rbac/ - curl script and two html files

---
+++ apis/sql/python/alt_graph/data/rbac/curl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles > roles.html
+
+curl https://docs.microsoft.com/en-us/azure/active-directory/roles/permissions-reference > permissions.html
+
+echo 'done'
+
