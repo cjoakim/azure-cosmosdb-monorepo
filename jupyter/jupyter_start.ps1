e83d2638071fadbad56822c2a5902d1441979a54
Chris Joakim <christopher.joakim@gmail.com>
2022-06-25 06:08:06 -0400

    deleted the jupyter/ dir at repo root

--- jupyter/jupyter_start.ps1
+++
@@ -1,9 +0,0 @@
-
-# List the available kernels, then start jupyter.
-# Chris Joakim, Microsoft, January 2022
-
-echo 'listing available kernels ...'
-jupyter kernelspec list
-
-echo 'starting jupyter notebook ...'
-jupyter notebook
