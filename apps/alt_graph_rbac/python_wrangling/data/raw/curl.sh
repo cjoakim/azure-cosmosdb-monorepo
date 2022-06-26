dea50c868b3544d0ef302d9ff94278f3b55de2e6
Chris Joakim <christopher.joakim@gmail.com>
2022-06-26 06:32:01 -0400

    apps/alt_graph_rbac/python_wrangling/data/raw/

---
+++ apps/alt_graph_rbac/python_wrangling/data/raw/curl.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl https://docs.microsoft.com/en-us/azure/role-based-access-control/built-in-roles > azure_roles.html
+
+curl https://docs.microsoft.com/en-us/azure/active-directory/roles/permissions-reference > azure_ad_roles_permissions.html
+
+echo 'done'
+
